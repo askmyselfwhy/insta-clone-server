18719fa7d65dde8448fd32e2744c1ea14759d74e
Roman Yefimov <romanefimov2@gmail.com>
2019-06-10 04:13:12 +0300

    Fixed route

--- routes/posts.js
+++ routes/posts.js
@@ -4,9 +4,9 @@ var postsController = require('../controllers/posts')
 
 // Return list of posts
 router.get('/', postsController.all)
+
 // Return post by id
 router.get('/:id', postsController.findById)
-router.post('/posts', postsController.insert)
 // Change post info
 router.put('/:id', postsController.update)
 // Delete the post
